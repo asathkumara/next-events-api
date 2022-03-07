56b6145049c7222cf38f9975974c58d1fbb281f2
asathkumara <sathkumaraasel@gmail.com>
2022-03-06 19:05:47 -0800

    Added swagger docs to messages, and added getGroupMessages method to group repo

--- pages/api/groups/[groupName]/messages/[messageID]/index.js
+++ pages/api/groups/[groupName]/messages/[messageID]/index.js
@@ -1,5 +1,43 @@
 import {editMessage} from "../../../../../../lib/repositories/MessageRepository";
 
+/**
+ * @swagger
+ *
+ * /api/groups/{groupName}/messages/{messageID}:
+ *
+ *   patch:
+ *     summary: Edit a message
+ *     description: Edit a message
+ *     tags:
+ *       - Group
+ *     parameters:
+ *       - in: path
+ *         name: groupName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The name of the group
+ *       - in: path
+ *         name: messageID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The BSON ID of the message
+ *     requestBody:
+ *       description:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Message"
+ *           example:
+ *             contents: Scratch that, something came up. I'll meet you tomorrow.
+ *     responses:
+ *       200:
+ *         description: Message was edited successfully.
+ *
+ *
+ *  */
 const handler = async (request, response) => {
 
     let result = {acknowledged: false, message: "Only PATCH permitted."};
--- pages/api/groups/[groupName]/messages/index.js
+++ pages/api/groups/[groupName]/messages/index.js
@@ -1,18 +1,80 @@
 import {createMessage, getAll} from "../../../../../lib/repositories/MessageRepository";
-import {addMessage} from "../../../../../lib/repositories/GroupRepository";
+import {addMessage, getGroupMessages} from "../../../../../lib/repositories/GroupRepository";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Message:
+ *       type: object
+ *       properties:
+ *         userID:
+ *           type: string
+ *           description: The BSON ID of the user sending the message
+ *         contents:
+ *           type: string
+ *           description: The message contents
+ *
+ * /api/groups/{groupName}/messages:
+ *   get:
+ *     summary: Returns all the messages sent to the group
+ *     description: Returns all the messages sent to the group
+ *     tags:
+ *       - Group
+ *     parameters:
+ *       - in: path
+ *         name: groupName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The name of the group
+ *     responses:
+ *       200:
+ *         description: Groups were retrieved successfully.
+ *
+ *   post:
+ *     summary: Creates a new message
+ *     description: Creates a new message
+ *     tags:
+ *       - Group
+ *     parameters:
+ *       - in: path
+ *         name: groupName
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The name of the group
+ *     requestBody:
+ *       description:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Message"
+ *           example:
+ *             userID: 6224fe098c103c8a0a8f1859
+ *             contents: I'll meet you after class
+ *     responses:
+ *       201:
+ *         description: Message was sent successfully.
+ *
+ *
+ *  */
 const handler = async (request, response) => {
 
     let result = {acknowledged: false, message: "Only GET / POST permitted."};
+    let { groupName } = request.query;
 
+    /**
+     * TO-DO: Parse message ids from get group messages to actual messages.
+     */
     switch (request.method)
     {
         case "GET":
-            let messages = await getAll();
+            let messages = await getGroupMessages({name: groupName});
             response.status(200).json(messages);
 
         case "POST":
-            let { groupName } = request.query;
             let { insertedId } = await createMessage(request.body);
             result = await addMessage(insertedId, groupName);
 
